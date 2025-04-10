116b9eeba204517d421956bdc41c3fb3ac8d3759
full-video-a11y <adsh8230@gmail.com>
2025-04-10 13:43:12 +0100

    index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as DOM from './dom.js';
+import * as State from './state.js';
+import * as Router from './router.js';
+import * as Events from './events.js';
+
+export {
+  DOM,
+  State,
+  Router,
+  Events
+};
