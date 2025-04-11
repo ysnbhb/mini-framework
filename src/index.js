a3ef7b0659846e64a6823c2f7ef8ccf1bda5926f
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-04-11 18:51:13 +0100

    router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import * as DOM from './dom.js';
-import * as State from './state.js';
-import * as Router from './router.js';
-import * as Events from './events.js';
+import * as DOM from "./dom.js";
+import * as State from "./state.js";
+import * as Router from "./router.js";
+import * as Events from "./events.js";
 
-export {
-  DOM,
-  State,
-  Router,
-  Events
-};
+export { DOM, State, Router, Events };
