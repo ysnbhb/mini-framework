0d0143865021ea814fb0d146197b4a711f63825e
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-04-11 18:50:23 +0100

    router

---
+++ src/router.js
@@ -0,0 +1,30 @@
+class Router {
+  constructor() {
+    this.rout = {};
+    this.document = document;
+  }
+  render() {
+    const path = this.getPath();
+    const func = this.rout[path];
+    if (!func) {
+      return;
+    }
+    func();
+  }
+  getPath() {
+    return this.document.location.pathname;
+  }
+  init() {
+    this.document.addEventListener("popstate", () => {
+      this.render();
+    });
+    this.render();
+  }
+  defined(path, func) {
+    const func = this.rout[path];
+    if (func) {
+      throw new Error("path arleady used");
+    }
+    this.rout[path] = func;
+  }
+}
